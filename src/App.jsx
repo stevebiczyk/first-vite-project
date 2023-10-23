3283bde2092044909b16b7f2536af3aebda85b19
stevebiczyk <stevebiczyk@gmail.com>
2023-10-23 20:33:00 +0100

    rendering lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
 const App = () => {
-  let x = "Hello World";
-  let array = ["user1", "user2", "user3", "user4", "user5"];
-  return (
-    <div>
-      {x}
-      {array.map((user) => {
-        return <h2>{user}</h2>;
-      })}
-    </div>
-  );
+  let data = "second";
+  return <div>{data === "first" ? <h1>first</h1> : <h1>second</h1>}</div>;
 };
 
 export default App;
