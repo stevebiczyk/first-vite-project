ec1f957c3a129aa5e7a1b72d7a82eb9081da669f
stevebiczyk <stevebiczyk@gmail.com>
2023-10-24 16:18:27 +0100

    Added basic app functions to Todo.jsx

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,15 +1,40 @@
+import { useState, useRef } from "react";
 import "./CSS/Todo.css";
+import { useEffect } from "react";
+
+let count = 0;
 const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
+  const addTodo = () => {
+    setTodos([
+      ...todos,
+      { number: count++, text: inputRef.current.value, display: "" },
+    ]);
+    inputRef.current.value = "";
+  };
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
   return (
     <div className="todo">
       <div className="todo-header">To-Do List</div>
       <div className="todo-add">
         <input
+          ref={inputRef}
           type="text"
           placeholder="Add a new task"
           className="todo-input"
         />
-        <div className="todo-add-btn">Add</div>
+        <div
+          onClick={() => {
+            addTodo();
+          }}
+          className="todo-add-btn"
+        >
+          Add
+        </div>
         <div className="todo-list"></div>
       </div>
     </div>
