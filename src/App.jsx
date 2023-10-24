d413cddc34839f9e044cad8825cf453b60730c4d
stevebiczyk <stevebiczyk@gmail.com>
2023-10-24 11:49:26 +0100

    React useRef hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import FirstComponent from "./components/FirstComponent";
 
 const App = () => {
-  const [x, setX] = useState(0);
-
-  const btnClick = () => {
-    console.log("Button clicked");
-    setX(x + 1);
-    console.log(x);
-  };
+  const [data, setData] = useState([]);
+  const inputRef = useRef(null);
   return (
     <div>
+      <input ref={inputRef} type="text" />
       <button
         onClick={() => {
-          btnClick();
+          setData([...data, inputRef.current.value]);
         }}
       >
-        Click me
+        Submit
       </button>
-      <FirstComponent data={x} fn={setX} />
+      {data.map((item, index) => {
+        return <h2 key={index}>{item}</h2>;
+      })}
     </div>
   );
 };
